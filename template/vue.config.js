c65d8a0b9ddba9eee7b32c01a34a341016b61869
徐杰辉 <xujiehui@xujiehuideMacBook-Pro.local>
2020-03-05 15:05:11 +0800

    show complie time

--- template/vue.config.js
+++ template/vue.config.js
@@ -1,7 +1,15 @@
+const OutputCompileTimeWebpackPlugin = require('output-compile-time-webpack-plugin')
+
 module.exports = {
   pages: {
     index: {
       entry: 'src/renderer/main.js'
     }
+  },
+  configureWebpack: config => {
+    const plugins = [
+      new OutputCompileTimeWebpackPlugin()
+    ]
+    config.plugins.push(...plugins)
   }
 }
